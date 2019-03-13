c6816203da7584764b07cc26b0af55a833e6544a
aviman <jitavijit@gmail.com>
2019-03-13 06:04:26 +0530

    Add files via upload

---
+++ addTwoNumbers.py
@@ -0,0 +1,10 @@
+#Program to add 2 numbers
+numberFirst = int(input("Enter First Number: "))
+numberSecond = int(input("Enter Second Number: "))
+
+numberAdd = numberFirst + numberSecond
+
+print("\nAddition : {}".format(numberAdd))
+#print("Addition : %d" %numberAdd)
+
+
